d69bbedbffa7003b6973e0fac5e2dc77427fb4d5
J Mackenzie <JMMackenzie@users.noreply.github.com>
2025-01-21 12:14:03 +1100

    Update GPT3Completion.py
    Move to new API

--- variant_generation/GPT3Completion.py
+++ variant_generation/GPT3Completion.py
@@ -2,13 +2,18 @@ import openai
 
 class GPT3Completion:
     def __init__(self, api_key):
-        openai.api_key = api_key
+        self.client = openai.OpenAI(api_key = api_key)        
 
     def generate_completions(self, prompt, model, temperature=0, max_tokens=2000):
-        completions = openai.Completion.create(
-            engine=model,
-            prompt=prompt,
+        response = self.client.chat.completions.create(
+            model=model,
+            messages=[ {
+                        "role" : "user",
+                        "content": prompt,
+                        },
+                      # could also have a system prompt if desired
+                     ],
             temperature=temperature,
             max_tokens=max_tokens
         )
-        return completions.choices[0].text
+        return completions.choices[0].message.content.strip()
